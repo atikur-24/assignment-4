b6d416e80fa37f487670ed4a4a85b1dd70d411f8
Atikur <atikurrahman7223@gmail.com>
2023-02-07 10:47:17 +0600

    solve problem 3

--- script.js
+++ script.js
@@ -7,7 +7,8 @@
     const subtraction = division - 5;
     return subtraction;
 }
-// console.log(mindGame(5));
+// const result = mindGame(5);
+// console.log(result);
 
 /* input 50 number with calculation 3 multiplication,
    10 addition , 2 division, 5 subtraction */
@@ -18,7 +19,8 @@ function mindGame(number) {
     const subtraction = division - 5;
     return subtraction;
 }
-// console.log(mindGame(50));
+// const result2 = mindGame(50);
+// console.log(result2);
 
 /* input 33 number with calculation 3 multiplication,
    10 addition , 2 division, 5 subtraction */
@@ -29,9 +31,10 @@ function mindGame(number) {
     const subtraction = division - 5;
     return subtraction;
 }
-// console.log(mindGame(33));
+// const result3 = mindGame(33);
+// console.log(result3);
 
-//================ PROBLEM SOLVE 2 ================
+//===================== PROBLEM SOLVE 2 =====================
 
 // 'phero' text length find even or odd 
 function evenOdd(string) {
@@ -41,7 +44,8 @@ function evenOdd(string) {
         return 'odd'
     }
 }
-// console.log(evenOdd('phero'));
+// const result = evenOdd('phero');
+// console.log(result);
 
 // 'Batch7' text length find even or odd
 function evenOdd(string) {
@@ -51,7 +55,8 @@ function evenOdd(string) {
         return 'odd'
     }
 }
-// console.log(evenOdd('Batch7'));
+// const result2 = evenOdd('Batch7');
+// console.log(result2);
 
 
 // 'chatgpt' text length find even or odd
@@ -62,4 +67,22 @@ function evenOdd(string) {
         return 'odd'
     }
 }
-// console.log(evenOdd('chatgpt'));
+// const result3 = evenOdd('chatgpt');
+// console.log(result3);
+
+//===================== PROBLEM SOLVE 3 =====================
+
+// 
+function isLGSeven(anyNumber) {
+    const result = anyNumber - 7;
+    if ( result >= 7) {
+        const multiplication = anyNumber * 2;
+        console.log(multiplication);
+        // return multiplication
+    } else {
+        // return result;
+        console.log(result)
+    }
+}
+const rr = isLGSeven(6);
+// console.log(rr);
