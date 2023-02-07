dd35194546462d88232eff0133b80c65289eb8f2
Atikur <atikurrahman7223@gmail.com>
2023-02-07 11:38:59 +0600

    fixed looping

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* input 5 number with calculation 3 multiplication,
-   10 addition , 2 division, 5 subtraction */
+// any input parameter number with calculation 3 multiplication, 10 addition , 2 division and 5 subtraction
    function mindGame(number) {
     const multiplication = number * 3;
     const addition = multiplication + 10;
@@ -10,79 +9,49 @@
 // const result = mindGame(5);
 // console.log(result);
 
-/* input 50 number with calculation 3 multiplication,
-   10 addition , 2 division, 5 subtraction */
-function mindGame(number) {
-    const multiplication = number * 3;
-    const addition = multiplication + 10;
-    const division = addition / 2;
-    const subtraction = division - 5;
-    return subtraction;
-}
-// const result2 = mindGame(50);
-// console.log(result2);
 
-/* input 33 number with calculation 3 multiplication,
-   10 addition , 2 division, 5 subtraction */
-   function mindGame(number) {
-    const multiplication = number * 3;
-    const addition = multiplication + 10;
-    const division = addition / 2;
-    const subtraction = division - 5;
-    return subtraction;
-}
-// const result3 = mindGame(33);
-// console.log(result3);
 
-//===================== PROBLEM SOLVE 2 =====================
-
-// 'phero' text length find even or odd 
-function evenOdd(string) {
-    if (string.length % 2 === 0) {
-        return 'even'
+// finding which parameter text length the number even or odd 
+function evenOdd(name) {
+    if (name.length % 2 === 0) {
+        return 'even';
     } else {
-        return 'odd'
+        return 'odd';
     }
 }
 // const result = evenOdd('phero');
 // console.log(result);
 
-// 'Batch7' text length find even or odd
-function evenOdd(string) {
-    if (string.length % 2 === 0) {
-        return 'even'
-    } else {
-        return 'odd'
-    }
-}
-// const result2 = evenOdd('Batch7');
-// console.log(result2);
 
 
-// 'chatgpt' text length find even or odd
-function evenOdd(string) {
-    if (string.length % 2 === 0) {
-        return 'even'
-    } else {
-        return 'odd'
-    }
-}
-// const result3 = evenOdd('chatgpt');
-// console.log(result3);
-
-//===================== PROBLEM SOLVE 3 =====================
-
-// 
+// checking parameter number grater than and equal 7 or less than 7
 function isLGSeven(anyNumber) {
     const result = anyNumber - 7;
     if ( result >= 7) {
         const multiplication = anyNumber * 2;
-        console.log(multiplication);
-        // return multiplication
+        return multiplication;
     } else {
-        // return result;
-        console.log(result)
+        return result;
     }
 }
-const rr = isLGSeven(6);
+// const rr = isLGSeven(15);
 // console.log(rr);
+
+
+
+// finding array in parameter positive or negative numbers
+function findingBadData(ages) {
+    for (let i = 0; i < ages.length; i++) {
+        const age = ages[i]
+        if (age  < 0) {
+            console.log(age)
+        }
+        else {
+            console.log(age)
+        }
+    }
+}
+
+const ages = [1, 2, 5, 0, -5];
+const finalResult = findingBadData(ages);
+// console.log(finalResult);
