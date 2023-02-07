8f3add8b58c60e6e5b51d728fe41af197cbb6358
Atikur <atikurrahman7223@gmail.com>
2023-02-07 08:58:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+/* input 5 number with calculation 3 multiplication,
+   10 addition , 2 division, 5 subtraction */
+   function mindGame(number) {
+    const multiplication = number * 3;
+    const addition = multiplication + 10;
+    const division = addition / 2;
+    const subtraction = division - 5;
+    return subtraction;
+}
+// console.log(mindGame(5));
+
+/* input 50 number with calculation 3 multiplication,
+   10 addition , 2 division, 5 subtraction */
+function mindGame(number) {
+    const multiplication = number * 3;
+    const addition = multiplication + 10;
+    const division = addition / 2;
+    const subtraction = division - 5;
+    return subtraction;
+}
+// console.log(mindGame(50));
+
+/* input 33 number with calculation 3 multiplication,
+   10 addition , 2 division, 5 subtraction */
+   function mindGame(number) {
+    const multiplication = number * 3;
+    const addition = multiplication + 10;
+    const division = addition / 2;
+    const subtraction = division - 5;
+    return subtraction;
+}
+// console.log(mindGame(33));
+
+//================ PROBLEM SOLVE 2 ================
+
+// 'phero' text length find even or odd 
+function evenOdd(string) {
+    if (string.length % 2 === 0) {
+        return 'even'
+    } else {
+        return 'odd'
+    }
+}
+// console.log(evenOdd('phero'));
+
+// 'Batch7' text length find even or odd
+function evenOdd(string) {
+    if (string.length % 2 === 0) {
+        return 'even'
+    } else {
+        return 'odd'
+    }
+}
+// console.log(evenOdd('Batch7'));
+
+
+// 'chatgpt' text length find even or odd
+function evenOdd(string) {
+    if (string.length % 2 === 0) {
+        return 'even'
+    } else {
+        return 'odd'
+    }
+}
+// console.log(evenOdd('chatgpt'));
